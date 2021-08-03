97f58f69fc1914e48218bda39072f84869f5040c
Pablo Ortega <87501612+AndrePablo@users.noreply.github.com>
2021-08-02 22:23:31 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PathfindingVisualizer from "./PathFinder/pathfindingVisualizer";
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
